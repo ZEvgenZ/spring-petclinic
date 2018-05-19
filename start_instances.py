f082854cee0de5dd50e93254f334503355565966
zevgenz <ezagoruyko@gmail.com>
2018-05-19 13:25:46 +0300

    modify start_instances.py

--- start_instances.py
+++ start_instances.py
@@ -3,71 +3,39 @@ import boto3
 
 ec2 = boto3.resource('ec2', region_name='us-east-2')
 
+#IP-address range
+ip_addr = ["10.0.10.100", "10.0.10.200"]
+
 f_hosts=open('hosts', mode='w+')
 f_hosts.write("[hosts]\n")
 f_hosts.write("APP_VM ansible_ssh_host=10.0.10.200 ansible_ssh_user=ec2-user\n")
 f_hosts.write("DB_VM ansible_ssh_host=10.0.10.100 ansible_ssh_user=ec2-user\n")
 f_hosts.close()
 
+
 # find image id ami-976152f2 / us-east-2
 # Create instances
-instances = ec2.create_instances(
+def create_instance(ip):
+    instances = ec2.create_instances(
     ImageId='ami-2a0f324f', 
     InstanceType='t2.micro',
     KeyName='aws_pair_keys', 
     MaxCount=1,
     MinCount=1,
-    #SubnetId = "subnet-fa255292",
-    #SecurityGroupIds = ["sg-c4d22aae"],
-    #AssociatePublicIpAddress = True
-    #Tags=[{'Key': 'Name', 'Value': 'db_host'}],
-    NetworkInterfaces=[{'SubnetId': "subnet-fa255292", 'DeviceIndex': 0, 'AssociatePublicIpAddress': True, 'PrivateIpAddress': '10.0.10.200', 'Groups': ["sg-c4d22aae",]}],
-    #TagSpecifications = [
-    #        {
-    #            
-    #            'Tags': [
-    #                {
-    #                    'Key': 'Name',
-    #                    'Value': "db_host"
-    #                },
-    #            ]
-    #        },
-    #    ]
-)   
-instances[0].wait_until_running()
-instances = ec2.create_instances(
-    ImageId='ami-2a0f324f', 
-    InstanceType='t2.micro',
-    KeyName='aws_pair_keys', 
-    MaxCount=1,
-    MinCount=1,
-    #SubnetId = "subnet-fa255292",
-    #SecurityGroupIds = ["sg-c4d22aae"],
-    #AssociatePublicIpAddress = False
-    #Tags=[{'Key': 'Name', 'Value': 'app_host'}],
-    NetworkInterfaces=[{'SubnetId': "subnet-fa255292", 'DeviceIndex': 0, 'AssociatePublicIpAddress': True, 'PrivateIpAddress': '10.0.10.100', 'Groups': ["sg-c4d22aae",]}],
-    #TagSpecifications = [
-    #        {
-    #            
-    #            'Tags': [
-    #                {
-    #                    'Key': 'Name',
-    #                    'Value': "app_host"
-    #                },
-    #            ]
-    #        },
-    #    ]
-)
-instances[0].wait_until_running()
-print(instances[0].id)
+    NetworkInterfaces=[{'SubnetId': "subnet-fa255292", 
+                        'DeviceIndex': 0, 
+                        'AssociatePublicIpAddress': True,
+                        'PrivateIpAddress': ip,
+                        'Groups': ["sg-c4d22aae",]
+                        }],
+        )
+    print(instances[0].id)  
+    return instances[0].wait_until_running()
+
+
+for ip in ip_addr:
+    create_instance(ip)
+
 
 for instance in ec2.instances.all():
     print (instance.id, instance.state)
-
-#f_hosts=open('hosts', mode='w+')
-#f_hosts.write("[hosts]\n")
-#f_hosts.write("APP_VM ansible_ssh_host=10.0.10.200 ansible_ssh_user=ec2-user\n")
-#f_hosts.write("DB_VM ansible_ssh_host=10.0.10.100 ansible_ssh_user=ec2-user\n")
-#f_hosts.close()
-
-#f_hosts=open('/etc/ansible/hosts', mode='a')

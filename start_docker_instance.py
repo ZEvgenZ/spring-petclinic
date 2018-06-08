a24221cf9c5080a1293a7d80a1c94f0d7e357aa9
zevgenz <ezagoruyko@gmail.com>
2018-06-08 21:53:42 +0300

    again and again mod 1

--- start_docker_instance.py
+++ start_docker_instance.py
@@ -33,11 +33,11 @@ for ip in ip_addr:
     create_instance(ip)
 
 
-for instance in ec2.instances.all():
-    #print (instance.id, instance.state)
-    #hosts-file for ansible
+for instance in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]):
+    print (instance.id, instance.state, instance.public_ip_address)
+    ######hosts-file for ansible
     f_hosts=open('hosts', mode='w+')
     f_hosts.write("[hosts]\n")
-    #f_hosts.write("APP_VM ansible_ssh_host=10.0.10.200 ansible_ssh_user=ec2-user\n")
+    #####f_hosts.write("APP_VM ansible_ssh_host=10.0.10.200 ansible_ssh_user=ec2-user\n")
     f_hosts.write("DB_VM ansible_ssh_host=" + instance.public_ip_address + "ansible_ssh_user=ec2-user\n")
     f_hosts.close()
